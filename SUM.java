0b1a4a0742d512b71b48d2908d86083a5990634a
YellaUdayKumar <150266430+YellaUdayKumar@users.noreply.github.com>
2023-11-17 10:33:16 +0530

    Update SUM.java

--- SUM.java
+++ SUM.java
@@ -8,15 +8,15 @@ public class SUM
 		do 
 		{
 			
-			 Scanner sc = new Scanner(System.in);
-			 System.out.println("enter any two  number");
-			 int a =sc.nextInt();
+			 Scanner sc = new Scanner(System.in); 
+			 System.out.println("enter any two  number"); // user input
+			 int a =sc.nextInt(); 
 			 int b =sc.nextInt();
 			 
 			 int sum=a+b;
-			 System.out.println("sum of two numbers is: " +sum);
+			 System.out.println("sum of two numbers is: " +sum); // sum of the two numbers
 		
-		      System.out.println("Do you want to continue for another Employee if yes input 1");
+		      System.out.println("Do you want to continue for another Employee if yes input 1"); // enter 1 it continue the program
 				
 		      int choice=sc.nextInt();
 				if(choice!= 1)
